261af63fed913bd6898df1ded795b395fcc7604a
ismoil793 <ismoil.793@gmail.com>
2020-05-21 21:14:34 +0500

    react heroku routing issue

--- server/server.js
+++ server/server.js
@@ -21,6 +21,18 @@ const {auth} = require("./middleware/auth");
 app.use(bodyParser.json());
 app.use(cookiesParser());
 
+
+if (process.env.NODE_ENV === 'production') {
+   // Exprees will serve up production assets
+   app.use(express.static('client/build'));
+
+   // Express serve up index.html file if it doesn't recognize route
+   const path = require('path');
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+}
+
 // app.use(express.static("client/build"));  // needed for production
 
 
